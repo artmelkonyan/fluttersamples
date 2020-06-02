74d933c8b0ac5efb9ea187559f52e4be797f2197
Arthur Melkonyan <arthurmelkonyan@gmail.com>
2020-06-02 14:09:21 +0400

    Added CHopper app

---
+++ src/chopperapp/lib/data/PostApiService.dart
@@ -0,0 +1,18 @@
+import 'package:chopper/chopper.dart';
+
+part 'PostApiService.Chopper.dart';
+
+@ChopperApi(baseUrl: 'https://jsonplaceholder.typicode.com/posts')
+abstract class PostApiServie extends ChopperService {
+  
+  @Get()
+  Future<Response> getPosts();
+
+  @Get(path: "/{id}")
+  Future<Response> getPost(@Path("id")int id);
+
+  @Post()
+  Future<Response> postPost(
+    @Body() Map<String,dynamic> body,
+  );
+}
